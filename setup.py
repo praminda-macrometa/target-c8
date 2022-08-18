fa860dd821272ee44b345749e4a274f43571c8eb
Praminda <praminda.jayawardana@macrometa.com>
2022-08-18 14:51:31 +0530

    c8: Add c8 as requirement

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name='target-c8',
-    version='0.1.4',
-    description='Singer.io target for writing JSON Line files',
+    version='1.0.0',
+    description='Singer.io target for writing to C8DB collection',
     long_description=readme,
     long_description_content_type='text/markdown',
     author='Andy Huynh',
@@ -16,7 +16,12 @@ setup(
     keywords=["singer", "singer.io", "target", "etl"],
     classifiers=['Programming Language :: Python :: 3 :: Only'],
     py_modules=['target_c8'],
-    install_requires=['jsonschema==2.6.0', 'singer-python==5.8.0', 'adjust-precision-for-schema==0.3.3'],
+    install_requires=[
+        'jsonschema==2.6.0',
+        'singer-python==5.8.0',
+        'adjust-precision-for-schema==0.3.3',
+        'pyC8==0.17.1'
+    ],
     entry_points='''
           [console_scripts]
           target-c8=target_c8:main
